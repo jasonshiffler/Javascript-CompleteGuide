49382d41b42cb9bace8bc1e88e52cee3b98bb96f
jasonshiffler <jason.shiffler@q-free.com>
2021-08-07 17:58:35 -0400

    Added items from section 5 of the lecture to the Section sample js code since there isn't much new stuff

--- Section2-Basics/Section2-Basics.js
+++ Section2-Basics/Section2-Basics.js
@@ -28,11 +28,10 @@
 {
     var globalScope = "Var doesn't use block scope, only Global/Function scope";
 }
+console.log(globalScope); //This is a global variable
 
-console.log(globalScope);
-
-let notDefined;
-console.log(" notDefined has a value of undefined since it hasn't been initialized with a value " + notDefined);
+var reDeclared = "This is legal with var, a good reason to use let";
+var reDeclared = "This is legal with var, a good reason to use let";
 
 //Javascript primative types
 let num = 10;  //number
@@ -43,6 +42,21 @@ console.log(typeof(str));
 
 let bool = true; //boolean
 console.log(typeof(bool));
+
+console.log(`${undefined} is a primative type`);
+
+//Objects are not primatives.
+let obj  = {
+    name: 'Garrett',
+    age: 12
+}
+console.log(` Accessing object values with dot notation ${obj.name}`);
+console.log(`Accessing object values with the key name ${obj["name"]}`);
+
+//Arrays are objects with keys equal to the index.
+let array = [1, 2, 3];
+console.log(`Arrays are ${typeof(array)}s`); 
+
 
 //Strings can use either single or double quotes.
 let singleQuote = 'Single Quote String';
@@ -58,10 +72,13 @@ sayHelloTo('Bob');
 //function declarations are hoisted to the top of the program flow.
 function sayHelloTo(name) {
     console.log(`Var doesnt cause an error ${x}`);
-    //console.log(`Let does cause an error ${y}`);
+    //console.log(`Let does cause an error ${y}`);    
     
     var x = 5; //variable declartion(not the assignment) is hoisted to the top of the function.
     let y = 5; //let does not hoist the declaration. must be declared before use.
+    z = 5; //Javascript automatically adds the var keyword for you. Can turn on strict mode to prevent this behavior. 'use strict';
+    var undefined = 5; // strict mode also prevents this.
+    console.log(`This is crazy **** ${undefined}`);
 
     console.log(`Hello ${name}`);
     return name.toUpperCase();        
@@ -78,3 +95,33 @@ function scopeSample() {
 //Functions can be passed as function arguments.
 setTimeout(() => scopeSample(), 1000); //-.passes a function that calls the function
 setTimeout(scopeSample, 1500); //Tells JS which function to execute.
+
+//Special function to convert types.
+console.log(`parseInt Converts "10" to 10. ${typeof(parseInt("10"))}`);
+
+/*
+Multiline comment
+*/
+
+//Single line comment
+
+//Shorthand Operators
+incrementer = 7;
+incrementer += 12;
+incrementer -= 8;
+incrementer++;
+incrementer--;
+
+
+//undefined is its own type
+let undefinedVariable;
+console.log(`${undefinedVariable} is the default value of an unassigned variable and is of type ${undefined}`);
+
+//null is of type object
+let nullVariable = null;
+console.log(`${nullVariable} is a special value and is of type ${typeof(null)}`);
+
+//NaN is a number
+let notANumber= NaN;
+console.log(`${notANumber} is a special value and is of type ${typeof(notANumber)}`);
+
